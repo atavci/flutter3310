3b7c7b450a3e82d8445cf0489987f997145a2d61
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-06-28 02:54:38 +0530

    Add basic dialer functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:nokiapro/dialer.dart';
 import 'package:nokiapro/styles/app_colors.dart';
+import 'package:provider/provider.dart';
+
+import 'dialer_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +13,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Nokia Pro',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider<DialerModel>(
+      create: (context) => DialerModel(),
+      child: MaterialApp(
+        title: 'Nokia Pro',
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
@@ -31,20 +38,24 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: AppColors.bgColor,
       body: Stack(
+        alignment: Alignment.center,
         children: <Widget>[
           Positioned(
+            top: 100,
             left: 50,
             right: 50,
             child: Container(
-              height: MediaQuery.of(context).size.height / 3,
+              height: MediaQuery.of(context).size.height / 4,
               width: MediaQuery.of(context).size.width / 2,
               color: AppColors.greenScreenColor,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    'test',
-                    style: TextStyle(color: Colors.black,fontSize: 25),
+                  Consumer<DialerModel>(
+                    builder: (context, value, child) => Text(
+                      value.number,
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
                   )
                 ],
               ),
@@ -58,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     image: AssetImage('assets/images/nokiabig.png'),
                     fit: BoxFit.contain)),
           ),
+          Dialer(),
         ],
       ),
     );
